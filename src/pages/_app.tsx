88f307d07ce857e4b89afb2dca87d0625cac6fe0
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-09-29 15:53:05 +0100

    wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
+import { SWRConfig } from 'swr'
 import 'tailwindcss/tailwind.css'
 import { ThemeProvider } from 'next-themes'
 import Web3Provider from '@/components/Web3Provider'
 
+const swrConfig = {
+	fetcher: url => fetch(url).then(res => res.json()),
+}
+
 const App = ({ Component, pageProps }) => {
 	return (
 		<ThemeProvider attribute="class">
-			<Web3Provider>
-				<Component {...pageProps} />
-			</Web3Provider>
+			<SWRConfig value={swrConfig}>
+				<Web3Provider>
+					<Component {...pageProps} />
+				</Web3Provider>
+			</SWRConfig>
 		</ThemeProvider>
 	)
 }
